b506bdb2e7749c909ffc0a16c4849b376652f590
Anderson Lima <anderson.gadelha@zup.com.br>
2025-01-28 16:27:50 -0300

    chore: add base project

---
+++ src/main/java/com/zup/desafio_e_commerce/DesafioECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.zup.desafio_e_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioECommerceApplication.class, args);
+	}
+
+}
